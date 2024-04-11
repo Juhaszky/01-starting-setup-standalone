8a336acc5e214c960693573b70190ce84e78d9de
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2024-04-11 15:32:58 +0200

    feat(counter state): service removed, counter actions and reducer added

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,12 +1,15 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.actions';
 
 const initialState = 0;
 //newer versions
-// export const counterReducer = createReducer(
-//     initialState
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state, action) => state - action.value)
+);
 
 //older versions
-export function counterReducer(state = initialState) {
-    return state;
-}
+// export function counterReducer(state = initialState) {
+//     return state;
+// }
